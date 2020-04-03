2baf4aa255696450e234c9b126399861e2370a89
Sahil Nare <sahilnare78@gmail.com>
2020-04-03 20:11:36 +0530

    Other Shapes

--- static/js/game.js
+++ static/js/game.js
@@ -1,6 +1,6 @@
 
 window.addEventListener('DOMContentLoaded', function() {
-  
+
   var canvas = document.getElementById("renderCanvas"); // Get the canvas element
   var engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
 
@@ -18,7 +18,18 @@ window.addEventListener('DOMContentLoaded', function() {
     var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
 
     // Add and manipulate meshes in the scene
-    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter:2}, scene);
+    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameterX: 1, diameterY: 2, segments: 16}, scene);
+    sphere.position.y = 2;
+    var myBox = BABYLON.MeshBuilder.CreateBox("myBox", {width: 1, height: 1, depth: 2}, scene);
+    myBox.position.y = 0.5;
+    var myGround = BABYLON.MeshBuilder.CreateGround("myGround", {width: 6, height: 4, subdivisions: 4}, scene);
+    var myPoints = [
+      new BABYLON.Vector3(0, 0, 0),
+      new BABYLON.Vector3(0, 5, 5),
+      new BABYLON.Vector3(0, 5, 0)
+    ];
+    var lines = BABYLON.MeshBuilder.CreateLines("lines", {points: myPoints}, scene);
+    // var myPlane = BABYLON.MeshBuilder.CreatePlane("myPlane", {width: 5, height: 2, sideOrientation: BABYLON.Mesh.DOUBLESIDE}, scene);
 
     return scene;
   };
